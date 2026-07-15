438b02fccba9849bd1dd65499284f8760f7b2a5d
Ben Rempe <benrempe@limitlessgrowthguild.com>
2026-07-15 10:06:44 -0400

    feat: route playoff outlook

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import Analytics from "./pages/Analytics.tsx";
 import Standings from "./pages/Standings.tsx";
+import Playoff from "./pages/Playoff.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/analytics" element={<Analytics />} />
           <Route path="/standings" element={<Standings />} />
+          <Route path="/playoff" element={<Playoff />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
